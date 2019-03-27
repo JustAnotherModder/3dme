bb7a9e50ae863e1258f8b77d7321e23ffec71a5c
JustAnotherModder <48855422+JustAnotherModder@users.noreply.github.com>
2019-03-27 18:54:18 +1030

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,85 +1,125 @@
-JAM_Garage = {}
+-- begin Just Another Mod
+function GetSharedObject(obj) ESX = obj; end
 
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)	
+local playerName = ''
 
-function JAM_Garage:GetPlayerVehicles(identifier)	
-	local playerVehicles = {}
-	local data = MySQL.Sync.fetchAll("SELECT * FROM owned_vehicles WHERE owner=@identifier",{['@identifier'] = identifier})	
-	for key,val in pairs(data) do
-		local playerVehicle = json.decode(val.vehicle)
-		table.insert(playerVehicles, {owner = val.owner, veh = val.vehicle, vehicle = playerVehicle, plate = val.plate, state = val.state})
-	end
-	return playerVehicles
+function MeHandler(name, args)
+    if not ESX then return; end
+
+    local text = '* ' .. playerName
+    for i = 1,#args do
+        text = text .. ' ' .. args[i]
+    end
+    text = text .. ' *'
+    TriggerServerEvent('3dme:shareDisplay', text)
 end
 
-ESX.RegisterServerCallback('JAM_Garage:StoreVehicle', function(source, cb, vehicleProps)
-	local isFound = false
-	local xPlayer = ESX.GetPlayerFromId(source)
+RegisterCommand('me', MeHandler, playerName, args)
 
-	if not xPlayer then return; end
+function UpdateThread()
+    TriggerEvent('esx:getSharedObject', function(...) GetSharedObject(...); end);
 
-	local playerVehicles = JAM_Garage:GetPlayerVehicles(xPlayer.getIdentifier())
-	local plate = vehicleProps.plate
+    Citizen.Wait(1000)
 
-	for key,val in pairs(playerVehicles) do
-		if(plate == val.plate) then
-			local vehProps = json.encode(vehicleProps)
-			MySQL.Sync.execute("UPDATE owned_vehicles SET vehicle=@vehProps WHERE plate=@plate",{['@vehProps'] = vehProps, ['@plate'] = val.plate})
-			isFound = true
-			break
-		end
-	end
-	cb(isFound)
-end)
-
-ESX.RegisterServerCallback('JAM_Garage:GetVehicles', function(source, cb)
-	local xPlayer = ESX.GetPlayerFromId(source)
-
-	if not xPlayer then return; end
-
-	local vehicles = JAM_Garage:GetPlayerVehicles(xPlayer.getIdentifier())
-
-	cb(vehicles)
-end)
-
-RegisterNetEvent('JAM_Garage:ChangeState')
-AddEventHandler('JAM_Garage:ChangeState', function(plate, state)
-	local xPlayer = ESX.GetPlayerFromId(source)
-
-	if not xPlayer then return; end
-
-	local vehicles = JAM_Garage:GetPlayerVehicles(xPlayer.getIdentifier())
-	for key,val in pairs(vehicles) do
-		if(plate == val.plate) then
-			MySQL.Sync.execute("UPDATE owned_vehicles SET state =@state WHERE plate=@plate",{['@state'] = state , ['@plate'] = plate})
-			break
-		end		
-	end
-end)
-
--- Console = dofile("Console.lua")
-function JAM_Garage.Startup()
-	
-	-- print( Console:Start() )
-
-	local data = MySQL.Sync.fetchAll("SELECT * FROM owned_vehicles")
-	for key,val in pairs(data) do
-	  	if not val.state
-	  	then
-	   		MySQL.Sync.fetchAll("ALTER TABLE `owned_vehicles` ADD `state` int(11) NOT NULL;")
-	  	end
-	  	return
-	end
+    ESX.TriggerServerCallback('ReturnCharacterName', function(str)
+        playerName = str
+    end, source)
 end
 
--- function Console:Start()
---   if not self then return Console:Start(); end;
---   if self.started then return true, "Already started..."; end;
---   self.started = true
---   if    not Citizen or not Citizen.CreateThread then return false, "not Citizen or not Citizen.CreateThread"; end;
---   Citizen.CreateThread(Console.ThreadUpdate)
---   return true
--- end
+Citizen.CreateThread(function(...) UpdateThread(...); end)
+-- end JAM
 
-RegisterNetEvent('JAM_Garage:Startup')
-AddEventHandler('JAM_Garage:Startup', JAM_Garage.Startup)
+-- Settings
+
+local color = { r = 220, g = 220, b = 220, alpha = 255 } -- Color of the text 
+local font = 0 -- Font of the text
+local time = 7000 -- Duration of the display of the text : 1000ms = 1sec
+local background = {
+    enable = false,
+    color = { r = 35, g = 35, b = 35, alpha = 200 },
+}
+local chatMessage = true
+local dropShadow = false
+
+-- Don't touch
+local nbrDisplaying = 1
+
+RegisterNetEvent('3dme:triggerDisplay')
+AddEventHandler('3dme:triggerDisplay', function(text, source)
+    local offset = 1 + (nbrDisplaying*0.14)
+    Display(GetPlayerFromServerId(source), text, offset)
+end)
+
+function Display(mePlayer, text, offset)
+    local displaying = true
+
+    -- Chat message
+    if chatMessage then
+        local coordsMe = GetEntityCoords(GetPlayerPed(mePlayer), false)
+        local coords = GetEntityCoords(PlayerPedId(), false)
+        local dist = Vdist2(coordsMe, coords)
+        if dist < 2500 then
+            TriggerEvent('chat:addMessage', {
+                color = { color.r, color.g, color.b },
+                multiline = true,
+                args = { text}
+            })
+        end
+    end
+
+    Citizen.CreateThread(function()
+        Wait(time)
+        displaying = false
+    end)
+    Citizen.CreateThread(function()
+        nbrDisplaying = nbrDisplaying + 1
+        print(nbrDisplaying)
+        while displaying do
+            Wait(0)
+            local coordsMe = GetEntityCoords(GetPlayerPed(mePlayer), false)
+            local coords = GetEntityCoords(PlayerPedId(), false)
+            local dist = Vdist2(coordsMe, coords)
+            if dist < 2500 then
+                DrawText3D(coordsMe['x'], coordsMe['y'], coordsMe['z']+offset, text)
+            end
+        end
+        nbrDisplaying = nbrDisplaying - 1
+    end)
+end
+
+function DrawText3D(x,y,z, text)
+    local onScreen,_x,_y = World3dToScreen2d(x,y,z)
+    local px,py,pz = table.unpack(GetGameplayCamCoord())
+    local dist = GetDistanceBetweenCoords(px,py,pz, x,y,z, 1)
+ 
+    local scale = ((1/dist)*2)*(1/GetGameplayCamFov())*100
+
+    if onScreen then
+
+        -- Formalize the text
+        SetTextColour(color.r, color.g, color.b, color.alpha)
+        SetTextScale(0.0*scale, 0.55*scale)
+        SetTextFont(font)
+        SetTextProportional(1)
+        SetTextCentre(true)
+        if dropShadow then
+            SetTextDropshadow(10, 100, 100, 100, 255)
+        end
+
+        -- Calculate width and height
+        BeginTextCommandWidth("STRING")
+        AddTextComponentString(text)
+        local height = GetTextScaleHeight(0.55*scale, font)
+        local width = EndTextCommandGetWidth(font)
+
+        -- Diplay the text
+        SetTextEntry("STRING")
+        AddTextComponentString(text)
+        EndTextCommandDisplayText(_x, _y)
+
+        if background.enable then
+            DrawRect(_x, _y+scale/45, width, height, background.color.r, background.color.g, background.color.b , background.color.alpha)
+        end
+    end
+end
+
