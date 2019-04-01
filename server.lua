d8281714e92d45df12288d923dbf3ae868695b94
JustAnotherModder <48855422+JustAnotherModder@users.noreply.github.com>
2019-04-01 18:04:07 +1030

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,125 +1,40 @@
 -- begin Just Another Mod
-function GetSharedObject(obj) ESX = obj; end
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-local playerName = ''
-
-function MeHandler(name, args)
-    if not ESX then return; end
-
-    local text = '* ' .. playerName
-    for i = 1,#args do
-        text = text .. ' ' .. args[i]
-    end
-    text = text .. ' *'
-    TriggerServerEvent('3dme:shareDisplay', text)
+function ReturnCharacterHandler(sourceID)
+	local playername = ''
+	local data = MySQL.Sync.fetchAll("SELECT * FROM characters WHERE identifier=@identifier",{['@identifier'] = sourceID})
+	for key,val in pairs(data) do
+		playername = val.firstname .. " " .. val.lastname
+		return playername
+	end
 end
 
-RegisterCommand('me', MeHandler, playerName, args)
+ESX.RegisterServerCallback('ReturnCharacterName', function(source, cb)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local str = ReturnCharacterHandler(xPlayer.getIdentifier())
+	cb(str)
+end)	
+--- end JAM
 
-function UpdateThread()
-    TriggerEvent('esx:getSharedObject', function(...) GetSharedObject(...); end);
+local logEnabled = true
 
-    Citizen.Wait(1000)
-
-    ESX.TriggerServerCallback('ReturnCharacterName', function(str)
-        playerName = str
-    end, source)
-end
-
-Citizen.CreateThread(function(...) UpdateThread(...); end)
--- end JAM
-
--- Settings
-
-local color = { r = 220, g = 220, b = 220, alpha = 255 } -- Color of the text 
-local font = 0 -- Font of the text
-local time = 7000 -- Duration of the display of the text : 1000ms = 1sec
-local background = {
-    enable = false,
-    color = { r = 35, g = 35, b = 35, alpha = 200 },
-}
-local chatMessage = true
-local dropShadow = false
-
--- Don't touch
-local nbrDisplaying = 1
-
-RegisterNetEvent('3dme:triggerDisplay')
-AddEventHandler('3dme:triggerDisplay', function(text, source)
-    local offset = 1 + (nbrDisplaying*0.14)
-    Display(GetPlayerFromServerId(source), text, offset)
+RegisterServerEvent('3dme:shareDisplay')
+AddEventHandler('3dme:shareDisplay', function(text)
+	TriggerClientEvent('3dme:triggerDisplay', -1, text, source)
+	if logEnabled then
+		setLog(text, source)
+	end
 end)
 
-function Display(mePlayer, text, offset)
-    local displaying = true
+function setLog(text, source)
+	local time = os.date("%d/%m/%Y %X")
+	local name = GetPlayerName(source)
+	local identifier = GetPlayerIdentifiers(source)
+	local data = time .. ' : ' .. name .. ' - ' .. identifier[1] .. ' : ' .. text
 
-    -- Chat message
-    if chatMessage then
-        local coordsMe = GetEntityCoords(GetPlayerPed(mePlayer), false)
-        local coords = GetEntityCoords(PlayerPedId(), false)
-        local dist = Vdist2(coordsMe, coords)
-        if dist < 2500 then
-            TriggerEvent('chat:addMessage', {
-                color = { color.r, color.g, color.b },
-                multiline = true,
-                args = { text}
-            })
-        end
-    end
-
-    Citizen.CreateThread(function()
-        Wait(time)
-        displaying = false
-    end)
-    Citizen.CreateThread(function()
-        nbrDisplaying = nbrDisplaying + 1
-        print(nbrDisplaying)
-        while displaying do
-            Wait(0)
-            local coordsMe = GetEntityCoords(GetPlayerPed(mePlayer), false)
-            local coords = GetEntityCoords(PlayerPedId(), false)
-            local dist = Vdist2(coordsMe, coords)
-            if dist < 2500 then
-                DrawText3D(coordsMe['x'], coordsMe['y'], coordsMe['z']+offset, text)
-            end
-        end
-        nbrDisplaying = nbrDisplaying - 1
-    end)
-end
-
-function DrawText3D(x,y,z, text)
-    local onScreen,_x,_y = World3dToScreen2d(x,y,z)
-    local px,py,pz = table.unpack(GetGameplayCamCoord())
-    local dist = GetDistanceBetweenCoords(px,py,pz, x,y,z, 1)
- 
-    local scale = ((1/dist)*2)*(1/GetGameplayCamFov())*100
-
-    if onScreen then
-
-        -- Formalize the text
-        SetTextColour(color.r, color.g, color.b, color.alpha)
-        SetTextScale(0.0*scale, 0.55*scale)
-        SetTextFont(font)
-        SetTextProportional(1)
-        SetTextCentre(true)
-        if dropShadow then
-            SetTextDropshadow(10, 100, 100, 100, 255)
-        end
-
-        -- Calculate width and height
-        BeginTextCommandWidth("STRING")
-        AddTextComponentString(text)
-        local height = GetTextScaleHeight(0.55*scale, font)
-        local width = EndTextCommandGetWidth(font)
-
-        -- Diplay the text
-        SetTextEntry("STRING")
-        AddTextComponentString(text)
-        EndTextCommandDisplayText(_x, _y)
-
-        if background.enable then
-            DrawRect(_x, _y+scale/45, width, height, background.color.r, background.color.g, background.color.b , background.color.alpha)
-        end
-    end
+	local content = LoadResourceFile(GetCurrentResourceName(), "log.txt")
+	local newContent = content .. '\r\n' .. data
+	SaveResourceFile(GetCurrentResourceName(), "log.txt", newContent, -1)
 end
 
